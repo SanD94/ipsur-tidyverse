fc1ba917e0d65cf19241573f9b1edcd587d53cd8
Safa Andac <8544658+SanD94@users.noreply.github.com>
2024-06-05 11:36:07 +0200

    add moment generator function

--- R/utils.R
+++ R/utils.R
@@ -1,19 +1,20 @@
 ## Section 3 util functions
-# central moment
-moment <- function(x, d) {
+# central moment generating function
+# special case that x prob same
+moment_discrete <- function(x, d) {
   n <- length(x)
   m <- mean(x)
   1 / n * sum((x - m)^d)
 }
 
 
-
+# check e1071 module for details
 # unbiased kurtosis in SPSS
 # https://www.wikiwand.com/en/Kurtosis#Standard_unbiased_estimator
 kurtosis <- function(x) {
   n <- length(x)
-  m4 <- moment(x, 4)
-  m2 <- moment(x, 2)
+  m4 <- moment_discrete(x, 4)
+  m2 <- moment_discrete(x, 2)
 
   a <- (n - 1) / ((n - 2) * (n - 3))
   b <- (n + 1) * m4 / m2^2
@@ -27,4 +28,17 @@ skewness <- function(x) {
   a <- n / ((n - 1) * (n - 2))
   z <- scale(x)
   a * sum(z^3)
+}
+
+
+# Section 5
+# x is the parameter
+# prob function with a parameter x
+moment <- function(x, f_x) {
+  sum(x * f_x)
+}
+
+# moment generator function
+generate_moment <- function(t, f_x) {
+  moment(exp(t), f_x)
 }
