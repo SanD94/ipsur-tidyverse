d8574f32887ddd92d757048e5e28aa351b2ffcc5
Safa Andac <8544658+SanD94@users.noreply.github.com>
2024-06-14 10:43:45 +0200

    finish section 9

--- R/sec9.R
+++ R/sec9.R
@@ -57,3 +57,39 @@ qqPlot(x)
 qqPlot(y)
 
 t.test(len ~ supp, data = ToothGrowth, conf.level = 0.99)
+
+
+# Confidence Intervals for Proportions
+library(Hmisc)
+
+### Wald interval -- it applies CLT as approximation
+binconf(x = 7, n = 25, alpha = 0.05, method = "asymptotic")
+
+### Wilson interval
+binconf(x = 7, n = 25, alpha = 0.05, method = "wilson")
+
+### default method is wilson
+binconf(x = 7, n = 25)
+
+### prop.test supports one sided interval estimation
+prop.test(x = 7, n = 25, conf.level = 0.95, correct = FALSE)
+prop.test(x = 7, n = 25, alternative = "greater", correct = FALSE)
+
+
+### two sample p1 - p2 == 0
+prop.test(x = c(7, 11), n = c(25, 37), conf.level = 0.99, correct = FALSE)
+
+
+# Confidence Intervals for Variances
+x <- with(morley, Speed[Expt == 1])
+library(TeachingDemos)
+sigma.test(x, conf.level = 0.99)
+
+var.test(len ~ supp, data = ToothGrowth, conf.level = 0.99)
+
+
+# Sample Size and Margin of Error
+power.t.test(delta = 1, sd = 105, sig.level = 0.05,
+             type = "one.sample", power = 0.5)
+
+power.t.test(n = 20, delta = 1)
